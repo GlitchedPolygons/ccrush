9f82000b1c3571b125a39cb057d027542ac40f44
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-09-23 14:43:46 +0200

    Improved build flow (made build.sh unnecessary).

--- include/ccrush.h
+++ include/ccrush.h
@@ -57,12 +57,12 @@ extern "C" {
 /**
  * Ccrush version number.
  */
-#define CCRUSH_VERSION 101
+#define CCRUSH_VERSION 102
 
 /**
  * Ccrush version number (as a human-readable string).
  */
-#define CCRUSH_VERSION_STR "1.0.1"
+#define CCRUSH_VERSION_STR "1.0.2"
 
 /**
  * Default chunksize to use for compression/decompression buffers.
