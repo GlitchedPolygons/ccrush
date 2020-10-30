c95066690fba422b38fecb0a47fb229e11fad0b3
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-10-30 18:11:31 +0100

    Improved lib path loader.

--- csharp/ccrush.cs
+++ csharp/ccrush.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Runtime.InteropServices;
 
@@ -187,7 +188,6 @@ namespace GlitchedPolygons.CcrushSharp
         public CcrushSharpContext(string sharedLibPathOverride = null)
         {
             string os;
-            
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 os = "windows";
@@ -208,48 +208,54 @@ namespace GlitchedPolygons.CcrushSharp
                 throw new PlatformNotSupportedException("Unsupported OS");
             }
 
-            if (string.IsNullOrEmpty(sharedLibPathOverride))
+            if (!string.IsNullOrEmpty(sharedLibPathOverride))
             {
-                StringBuilder pathBuilder = new StringBuilder(256);
-                pathBuilder.Append("lib/");
-
-                switch (RuntimeInformation.ProcessArchitecture)
-                {
-                    case Architecture.X64:
-                        pathBuilder.Append("x64/");
-                        break;
-                    case Architecture.X86:
-                        pathBuilder.Append("x86/");
-                        break;
-                    case Architecture.Arm:
-                        pathBuilder.Append("armeabi-v7a/");
-                        break;
-                    case Architecture.Arm64:
-                        pathBuilder.Append("arm64-v8a/");
-                        break;
-                }
-
-                if (!Directory.Exists(pathBuilder.ToString()))
-                {
-                    throw new PlatformNotSupportedException($"Ccrush shared library not found in {pathBuilder.ToString()} and/or unsupported CPU architecture. Please don't forget to copy the ccrush shared libraries/DLL into the 'lib/{{CPU_ARCHITECTURE}}/{{OS}}/{{SHARED_LIB_FILE}}' folder of your output build directory.  https://github.com/GlitchedPolygons/ccrush/tree/master/csharp");
-                }
-
-                pathBuilder.Append(os);
-                pathBuilder.Append('/');
-
-                string[] l = Directory.GetFiles(pathBuilder.ToString());
-                if (l?.Length != 1)
-                {
-                    throw new FileLoadException("There should only be exactly one ccrush shared library file per supported platform!");
-                }
-
-                pathBuilder.Append(Path.GetFileName(l[0]));
-                LoadedLibraryPath = Path.GetFullPath(pathBuilder.ToString());
-                pathBuilder.Clear();
+                LoadedLibraryPath = sharedLibPathOverride;
             }
             else
             {
-                LoadedLibraryPath = sharedLibPathOverride;
+                string cpu;
+                
+                switch (RuntimeInformation.ProcessArchitecture)
+                {
+                    case Architecture.X64:
+                        cpu = "x64";
+                        break;
+                    case Architecture.X86:
+                        cpu = "x86";
+                        break;
+                    case Architecture.Arm:
+                        cpu = "armeabi-v7a";
+                        break;
+                    case Architecture.Arm64:
+                        cpu = "arm64-v8a";
+                        break;
+                    default:
+                        throw new PlatformNotSupportedException("CPU Architecture not supported!");
+                }
+
+                string path = Path.Combine(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location) ?? "."), "lib", cpu, os);
+
+                if (!Directory.Exists(path))
+                {
+                    throw new PlatformNotSupportedException($"Shared library not found in {path} and/or unsupported CPU architecture. Please don't forget to copy the shared libraries/DLL into the 'lib/{{CPU_ARCHITECTURE}}/{{OS}}/{{SHARED_LIB_FILE}}' folder of your output build directory. ");
+                }
+
+                bool found = false;
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    if (file.ToLower().Contains("ccrush"))
+                    {
+                        LoadedLibraryPath = Path.GetFullPath(Path.Combine(path, file));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    throw new FileLoadException($"Shared library not found in {path} and/or unsupported CPU architecture. Please don't forget to copy the shared libraries/DLL into the 'lib/{{CPU_ARCHITECTURE}}/{{OS}}/{{SHARED_LIB_FILE}}' folder of your output build directory. ");
+                }
             }
 
             lib = loadUtils.LoadLibrary(LoadedLibraryPath);
