d9f786f90d17293a92908e5c4dfe8b6e0831e8c7
Glitched Polygons <glitchedpolygons@protonmail.com>
2021-01-30 11:53:24 +0100

    Recompiled docs/

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['ccrush_5fcompress_19',['ccrush_compress',['../ccrush_8h.html#adff1cf1c550aeb17347d07667e9cec2b',1,'ccrush.h']]],
-  ['ccrush_5fcompress_5ffile_20',['ccrush_compress_file',['../ccrush_8h.html#a8c1e78b1c886648f32c6ada1794ebe84',1,'ccrush.h']]],
-  ['ccrush_5fdecompress_21',['ccrush_decompress',['../ccrush_8h.html#af7410beaf01480e35abaf72724ba0235',1,'ccrush.h']]],
-  ['ccrush_5fdecompress_5ffile_22',['ccrush_decompress_file',['../ccrush_8h.html#a6fa4f62a451ac157ff37376da86e0acb',1,'ccrush.h']]],
-  ['ccrush_5ffree_23',['ccrush_free',['../ccrush_8h.html#ab6b3260beb61d9c91b41ac23306aedd7',1,'ccrush.h']]],
-  ['ccrush_5fget_5fversion_5fnr_24',['ccrush_get_version_nr',['../ccrush_8h.html#aa869f0bf2107dbe27a2466b07ee958f2',1,'ccrush.h']]],
-  ['ccrush_5fget_5fversion_5fnr_5fstring_25',['ccrush_get_version_nr_string',['../ccrush_8h.html#ab213e6dac6d22a12d2b73155008066a8',1,'ccrush.h']]],
-  ['ccrush_5fnextpow2_26',['ccrush_nextpow2',['../ccrush_8h.html#a8ec95fda222e6620688ccb377df2f0f8',1,'ccrush.h']]]
+  ['ccrush_5fcompress',['ccrush_compress',['../ccrush_8h.html#adff1cf1c550aeb17347d07667e9cec2b',1,'ccrush.h']]],
+  ['ccrush_5fcompress_5ffile',['ccrush_compress_file',['../ccrush_8h.html#a8c1e78b1c886648f32c6ada1794ebe84',1,'ccrush.h']]],
+  ['ccrush_5fdecompress',['ccrush_decompress',['../ccrush_8h.html#af7410beaf01480e35abaf72724ba0235',1,'ccrush.h']]],
+  ['ccrush_5fdecompress_5ffile',['ccrush_decompress_file',['../ccrush_8h.html#a6fa4f62a451ac157ff37376da86e0acb',1,'ccrush.h']]],
+  ['ccrush_5ffree',['ccrush_free',['../ccrush_8h.html#ab6b3260beb61d9c91b41ac23306aedd7',1,'ccrush.h']]],
+  ['ccrush_5fget_5fversion_5fnr',['ccrush_get_version_nr',['../ccrush_8h.html#aa869f0bf2107dbe27a2466b07ee958f2',1,'ccrush.h']]],
+  ['ccrush_5fget_5fversion_5fnr_5fstring',['ccrush_get_version_nr_string',['../ccrush_8h.html#ab213e6dac6d22a12d2b73155008066a8',1,'ccrush.h']]],
+  ['ccrush_5fnextpow2',['ccrush_nextpow2',['../ccrush_8h.html#a8ec95fda222e6620688ccb377df2f0f8',1,'ccrush.h']]]
 ];
