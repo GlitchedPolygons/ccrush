64840f133a4ae64e10bf46ddad5fe94440dd41f9
Raphael Beck <raphael.beck@glitchedpolygons.com>
2025-05-27 17:49:31 +0200

    Implemented ccrush CLI.

--- src/main.c
+++ src/main.c
@@ -51,12 +51,16 @@ static const char HELP_TEXT[] = "\n"
                                 "------------- \n"
                                 "Compress and decompress data easily using zlib v%s.\n\n"
                                 "Usage:\n\n"
-                                "Pass the data to compress or decompress into the CLI's stdin (for example with a pipe).\n"
+                                "Pass the data to compress or decompress into the CLI's stdin (for example with a pipe).\n\n"
                                 "When decompressing, pass the \"-d\" argument to put ccrush into decompression mode.\n\n"
+                                "Optional parameters are:\n\n"
+                                "  -c\n  Sets the compression level to use when deflating the input data.\n  Must be a number between 0 and 9, where 0 means no compression at all and 9 is maximum compression (slowest).\n  Default value: 6\n\n"
+                                "  -b\n  Sets the buffer size (in KiB) to use for compressing/decompressing.\n  Must be less than 262144.\n  Default value: 256\n\n"
                                 "Compression examples:\n\n"
                                 "  cat file-to-compress.txt | ccrush > my-compressed-file.txt.zlib\n\n  ---\n  OR\n  ---\n\n"
                                 "  echo -n \"Why do we all have to wear these ridiculous ties?!\" | ccrush > my-compressed-file.txt.zlib\n\nn  ---\n  OR\n  ---\n\n"
                                 "  ccrush < cat file-to-compress.txt > my-compressed-file.txt.zlib\n\n  ---\n  OR\n  ---\n\n"
+                                "  ccrush -c 8 -b 1024 < cat file-to-compress.txt > my-compressed-file.txt.zlib\n\n"
                                 "Decompression examples:\n\n"
                                 "  cat my-compressed-file.txt.zlib | ccrush -d > decompressed-file.txt\n\n  ---\n  OR\n  ---\n\n"
                                 "  ccrush -d < cat my-compressed-file.txt.zlib\n\n"
@@ -70,20 +74,101 @@ static void print_help_text()
 
 int main(const int argc, char* argv[])
 {
+    int decompress = 0;
+    int compression_level = 6;
+    int buffer_size_kib = 256;
+
     for (int i = 1; i < argc; ++i)
     {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        const char* arg = argv[i];
+
+        if (strncmp(arg, "-h", 2) == 0 || strncmp(arg, "--help", 6) == 0)
         {
             print_help_text();
-            return 0;
+            return EXIT_SUCCESS;
+        }
+
+        if (strncmp(arg, "-d", 2) == 0 || strncmp(arg, "--decompress", 12) == 0)
+        {
+            decompress = 1;
+        }
+
+        if (strncmp(arg, "-c", 2) == 0 || strncmp(arg, "--compression-level", 19) == 0)
+        {
+            if (i == argc - 1)
+            {
+                fprintf(stderr, "Please specify a number between 0 and 9 after the \"-c\" argument.\n");
+                return CCRUSH_ERROR_INVALID_ARGS;
+            }
+
+            const unsigned long int level = strtoul(argv[i + 1], NULL, 10);
+
+            if (level > 9)
+            {
+                fprintf(stderr, "Compression level parameter must be a number between 0 and 9.\n");
+                return CCRUSH_ERROR_INVALID_ARGS;
+            }
+
+            compression_level = (int)level;
+        }
+
+        if (strncmp(arg, "-b", 2) == 0 || strncmp(arg, "--buffer-size", 13) == 0)
+        {
+            if (i == argc - 1)
+            {
+                fprintf(stderr, "Please specify a buffer size in KiB after the \"-b\" argument.\n");
+                return CCRUSH_ERROR_INVALID_ARGS;
+            }
+
+            const unsigned long int buffer_size = strtoul(argv[i + 1], NULL, 10);
+
+            if (buffer_size > CCRUSH_MAX_BUFFER_SIZE_KiB)
+            {
+                fprintf(stderr, "Buffer size too big; it must be between [1 KiB; 256 MiB].\n");
+                return CCRUSH_ERROR_INVALID_ARGS;
+            }
+
+            buffer_size_kib = (int)buffer_size;
         }
     }
 
-    if (argc < 2 && feof(stdin))
+    int r = -1;
+
+    if (decompress)
     {
-        print_help_text();
-        return 0;
+        r = ccrush_decompress_file_raw(stdin, stdout, (uint32_t)buffer_size_kib, 0, 1);
+    }
+    else
+    {
+        r = ccrush_compress_file_raw(stdin, stdout, (uint32_t)buffer_size_kib, compression_level, 0, 1);
     }
 
-    return 0;
+    switch (r)
+    {
+        case 0: {
+            break;
+        }
+        case CCRUSH_ERROR_INVALID_ARGS: {
+            fprintf(stderr, "Invalid arguments.\n");
+            break;
+        }
+        case CCRUSH_ERROR_FILE_ACCESS_FAILED: {
+            fprintf(stderr, "Input and/or output file access failed.\n");
+            break;
+        }
+        case CCRUSH_ERROR_OUT_OF_MEMORY: {
+            fprintf(stderr, "Out of memory.\n");
+            break;
+        }
+        case CCRUSH_ERROR_BUFFERSIZE_TOO_LARGE: {
+            fprintf(stderr, "Invalid buffer size argument; it must be in the range of [1 KiB; 256 MiB]\n");
+            break;
+        }
+        default: {
+            fprintf(stderr, "%s failed; %s returned error code: %d.\n", decompress ? "Decompression" : "Compression", decompress ? "ccrush_decompress_file_raw": "ccrush_compress_file_raw", r);
+            break;
+        }
+    }
+
+    return r;
 }
